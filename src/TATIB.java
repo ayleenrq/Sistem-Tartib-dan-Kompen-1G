4c7b4f73aaa99f9baf468bb97a5039aefe8afa58
admbaskara001 <adhambaskara2@gmail.com>
2023-09-22 12:39:46 +0700

    Pemindahan dari Devel

--- src/TATIB.java
+++ src/TATIB.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class TATIB {
     public static void main(String[] args) {
+        Scanner sc08 = new Scanner(System.in);
+        int jamAlfa, jamKompensasi;
+
+        System.out.println("=============================");
+        System.out.println("|     Hitung Jam Kompen     |");
+        System.out.println("=============================");
+        System.out.print("   Masukkan jam alfa  : ");
+        jamAlfa = sc08.nextInt();
         
+        jamKompensasi = jamAlfa * 2;
+
+        System.out.println("   Jam Kompensasi anda: " + jamKompensasi);   
+        System.out.println("=============================");     
     }
 }
