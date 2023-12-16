1e4193d42f9f967f922aa9756e47b5140a9e0ea2
marsyaaurel08 <sefiramarsya@gmail.com>
2023-12-16 19:35:09 +0700

    perbaruan fitur dosen

--- src/Final.java
+++ src/Final.java
@@ -369,188 +369,60 @@ public class Final {
                     System.out.println("[                  Dosen                   ]");
                     System.out.println("============================================");
                     System.out.println("[ MENU :                                   ]");
-                    System.out.println("[ 1. Input Jam Alfa                        ]");
-                    System.out.println("[ 2. Pembayaran Kompensasi                 ]");
-                    System.out.println("[ 3. Pelaporan Pelanggaran                 ]");
-                    System.out.println("[ 4. Feedback/Tanggapan                    ]");
-                    System.out.println("[ 5. Logout                                ]");
+                    System.out.println("[ 1. Pelaporan Pelanggaran                 ]");
+                    System.out.println("[ 2. Input Jam Alpa Mahasiswa              ]");
+                    System.out.println("[ 3. Daftar Jam Kompensasi                 ]");
+                    System.out.println("[ 4. Pembayaran Kompensasi                 ]");
+                    System.out.println("[ 5. Pembayaran Sanksi                     ]");
+                    System.out.println("[ 6. Feedback/Tanggapan                    ]");
+                    System.out.println("[ 7. Cek Data Mahasiswa                    ]");
+                    System.out.println("[ 8. Logout                                ]");
 
-                    System.out.print("Pilih menu (1/2/3/4/5?): ");
+
+
+
+                    System.out.print("Pilih menu (1/2/3?): ");
                     int menu = sc08.nextInt();
 
                     switch (menu) {
                         case 1:
-                            System.out.println("======================================================");
-                            System.out.println("[                   Input Jam Alfa                   ]");
-                            System.out.println("======================================================");
-                            System.out.print("Pilih mahasiswa (1: Adham, 2: Marsya, 3: Ayleen): ");
-                            int choice = sc08.nextInt();
-
-                            String mahasiswa = "";
-                            switch (choice) {
-                                case 1:
-                                    mahasiswa = "Adham";
-                                    break;
-                                case 2:
-                                    mahasiswa = "Marsya";
-                                    break;
-                                case 3:
-                                    mahasiswa = "Ayleen";
-                                    break;
-                                default:
-                                    System.out.println("Pilihan tidak valid.");
-                                    return;
-                            }
-
-                            System.out.print("Masukkan Jam Alfa untuk " + mahasiswa + ": ");
-                            int jamAlfa = sc08.nextInt();
-
-                            int jamKompen = jamAlfa * 2;
-                            System.out.println("\nBerhasil melakukan input jam alfa!");
-                            System.out.println("Nama Mahasiswa: " + mahasiswa);
-                            System.out.println("Jam Alfa: " + jamAlfa);
-                            System.out.println(mahasiswa + " mendapatkan jam kompensasi:  " + jamKompen + " jam");
-                            System.out.print("Exit (true) : ");
-                            exitBoolean = sc08.nextBoolean();
-                            System.out.println();
-                            if (exitBoolean) {
-                                break;
-                            }
+                            laporPelanggaran(sc08);
+                            break;
 
                         case 2:
-                            System.out.println("======================================================");
-                            System.out.println("[              Pembayaran Kompensasi                 ]");
-                            System.out.println("======================================================");
-                            System.out.print("Pilih mahasiswa (1: Adham, 2: Marsya, 3: Ayleen): ");
-                            choice = sc08.nextInt();
-
-                            mahasiswa = "";
-                            switch (choice) {
-                                case 1:
-                                    mahasiswa = "Adham";
-                                    break;
-                                case 2:
-                                    mahasiswa = "Marsya";
-                                    break;
-                                case 3:
-                                    mahasiswa = "Ayleen";
-                                    break;
-                                default:
-                                    System.out.println("Pilihan tidak valid.");
-                                    return;
-                            }
-                            System.out.println("Pilih cara pembayaran kompensasi:");
-                            System.out.println("1. Membantu Dosen");
-                            System.out.println("2. Membersihkan Ruangan");
-                            System.out.println("3. Membantu Panitia");
-                            System.out.println("4. Menjadi relawan sebagai petugas damkar");
-                            System.out.println("5. Menjadi Volunteer");
-                            System.out.print("Pilih Salah Satu (1/2/3): ");
-
-                            int pilihan = sc08.nextInt();
-                            System.out.print("Masukkan jam kompensasi yang harus dikerjakan: ");
-                            int jam = sc08.nextInt();
-                            System.out.println(mahasiswa + " diharuskan membayar kompensasi: ");
-
-                            switch (pilihan) {
-                                case 1:
-                                    System.out.println("Membantu dosen");
-                                    break;
-                                case 2:
-                                    System.out.println("Membersihkan ruangan");
-                                    break;
-                                case 3:
-                                    System.out.println("Membantu panitia");
-                                    break;
-                                case 4:
-                                    System.out.println("Menjadi relawan sebagai petugas damkar");
-                                    break;
-                                case 5:
-                                    System.out.println("Menjadi volunteer");
-                                    break;
-                                default:
-                                    System.out.println("Pilihan yang Anda masukkan tidak valid.");
-
-                            }
-                            System.out.println("Sebanyak " + jam + " jam");
-                            System.out.print("Exit (true) : ");
-                            exitBoolean = sc08.nextBoolean();
-                            System.out.println();
-                            if (exitBoolean) {
-                                break;
-                            }
+                            tambahkanJamAlpha(sc08);
+                            break;
 
                         case 3:
-                            System.out.println("======================================================");
-                            System.out.println("[              Pelaporan Pelanggaran                 ]");
-                            System.out.println("======================================================");
-                            System.out.print("Apakah Anda ingin melaporkan pelanggaran? (y/t): ");
-                            char p = sc08.next().charAt(0);
-
-                            if (p == 'y' || p == 'Y') {
-                                System.out.print("Masukkan nama mahasiswa: ");
-                                String namaMahasiswa = sc08.next();
-
-                                System.out.print("Masukkan pelanggaran yang dilakukan: ");
-                                String pelanggaran = sc08.next();
-
-                                System.out.print("Masukkan tingkat pelanggaran (berat/cukupBerat/sedang/ringan): ");
-                                String tingkatPelanggaran = sc08.next();
-
-                                System.out.println("Nama Mahasiswa: " + namaMahasiswa);
-                                System.out.println("Pelanggaran: " + pelanggaran);
-                                System.out.println("Tingkat Pelanggaran: " + tingkatPelanggaran + "\n");
-
-                                System.out.println(
-                                        "Terima Kasih, mahasiswa atas nama " + namaMahasiswa + " telah dilaporkan!");
-                            } else {
-                                System.out.println("Terima kasih. Tidak ada pelaporan pelanggaran.");
-                            }
-                            System.out.print("Exit (true) : ");
-                            exitBoolean = sc08.nextBoolean();
-                            System.out.println();
-                            if (exitBoolean) {
-                                break;
-                            }
+                            daftarKompensasiMahasiswa(sc08);
+                            break;
                         case 4:
-                            System.out.println("======================================================");
-                            System.out.println("[                  Feedback/Tanggapan                 ]");
-                            System.out.println("======================================================");
-                            System.out.print("Pilih mahasiswa (1: Adham, 2: Marsya, 3: Ayleen): ");
-                            choice = sc08.nextInt();
-
-                            mahasiswa = "";
-                            switch (choice) {
-                                case 1:
-                                    mahasiswa = "Adham";
-                                    break;
-                                case 2:
-                                    mahasiswa = "Marsya";
-                                    break;
-                                case 3:
-                                    mahasiswa = "Ayleen";
-                                    break;
-                                default:
-                                    System.out.println("Pilihan tidak valid.");
-                                    return;
-                            }
-                            System.out.println("Masukkan Feedback/Tanggapan: ");
-                            String tanggapan = sc08.next();
-                            System.out.println(mahasiswa + "taggapan dari dosen: " + tanggapan);
-                            System.out.print("Exit (true) : ");
-                            exitBoolean = sc08.nextBoolean();
-                            System.out.println();
-                            if (exitBoolean) {
-                                break;
-                            }
+                            tambahkanPembayaranKompen(sc08);
+                            break;
                         case 5:
-                            if (menu == 5) {
+                            tambahkanPembayaranTatib(sc08);
+                            break;
+                        case 6:
+                            feedback(sc08);
+                            break;
+                        case 7:
+                            tampilkanData();
+                            break;
+                        case 8:
+                            if (menu == 8) {
                                 System.out.println("Terimakasih telah menggunakan program ini!");
                                 System.out.println();
                                 exitBoolean = true;
-                                loginDosen = false;
+                                loginAdmin = false;
                                 break;
                             }
+                        default:
+                            System.out.println("============================================");
+                            System.out.println("[ Pilihan anda tidak valid!                ]");
+                            System.out.println("[ Silahkan masukkan menu yang valid!       ]");
+                            System.out.println("============================================");
+                            exitBoolean = false;
+                            break;
                     }
                 } while (exitBoolean != true);
 
